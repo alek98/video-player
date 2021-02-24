ecf72abd6978db8a557092c0b1dee5075ae3ec8c
alekalek <alek@spectra-alarm.com>
2021-02-24 21:57:49 +0100

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,77 @@
 <template>
-  <div id="app">
-    <div>
-    <h1>Video Player</h1>
-    <button id="chooseButton"> Choose Video </button>
-    </div>
-    <Player v-show='false'/>
-  </div>
+  <v-app>
+    <v-main>
+      <div id="app">
+        <div>
+          <h1>Video Player</h1>
+          <button id="chooseButton">Choose Video</button>
+        </div>
+        <Player v-show="false" />
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-
-import Player from './components/Player.vue'
+import Player from "./components/Player.vue";
 
 export default {
-  name: 'App',
-  components: {
-    Player
-  }
-}
-</script>
+  name: "App",
 
+  components: {
+    Player,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
 <style>
-body{
+#app {
   margin: 0px !important;
   padding: 0px !important;
-  /* background-image: linear-gradient(to bottom right, rgb(18, 194, 233), rgb(196, 113, 237), rgb(246, 79, 89)); */
-  background-image: url("./assets/background.jpg");
+  background-image: linear-gradient(to bottom right, rgb(23, 162, 194), rgb(167, 90, 206), rgb(209, 66, 75));
   background-repeat: no-repeat;
   background-size: cover;
-   background-attachment: fixed;
+  background-attachment: fixed;
   background-color: #0e153a;
 }
-@import url('https://fonts.googleapis.com/css?family=Poppins');
-h1{
-  font-family: 'Poppins';
-  color:#e2f3f5;
+@import url("https://fonts.googleapis.com/css?family=Poppins");
+h1 {
+  font-family: "Poppins";
+  color: #e2f3f5;
   font-size: 60px;
   font-weight: 100;
   text-align: center;
   margin-top: 70px;
   margin-bottom: 60px;
 }
-#chooseButton{
+#chooseButton {
   margin-left: auto;
   margin-right: auto;
   display: block;
-  font-family: 'Poppins';
-  color:#e2f3f5;
-  background-image:linear-gradient(to right,#3d5af1, #21a5bd);
-  border:0px;
+  font-family: "Poppins";
+  color: #e2f3f5;
+  background-image: linear-gradient(to right, #3d5af1, #21a5bd);
+  border: 0px;
   border-radius: 10px;
-  padding-inline:15px;
+  padding-inline: 15px;
   padding-block: 5px;
   font-size: 25px;
-   box-shadow:  1px 3px 10px 1px #f3f169;
+  box-shadow: 1px 3px 10px 1px #f3f169;
 }
-#chooseButton:hover{
+#chooseButton:hover {
   margin-left: auto;
   margin-right: auto;
   display: block;
-  font-family: 'Poppins';
-  color:#e2f3f5;
-  background-image:linear-gradient(to right,#3550da, #16a6c0);
-  border:0px;
+  font-family: "Poppins";
+  color: #e2f3f5;
+  background-image: linear-gradient(to right, #3550da, #16a6c0);
+  border: 0px;
   border-radius: 10px;
-  padding-inline:15px;
+  padding-inline: 15px;
   padding-block: 5px;
   font-size: 25px;
 }
-
 </style>
