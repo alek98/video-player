acfb72484344f8854d4b2288288521758d48c311
alek98 <alek@spectra-alarm.com>
2021-02-25 16:45:36 +0100

    add show controls and hide controls

--- src/components/PlayerCore.vue
+++ src/components/PlayerCore.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <div class="video-wrapper">
+    <v-container
+      fluid 
+      pa-0
+      ma-0
+      class="video-wrapper"
+      @mouseenter="showControls()"
+      @mouseleave="hideControls()"
+    >
       <video ref="videoPlayer">
         <source
           src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4#t=0.5"
@@ -8,7 +15,11 @@
       </video>
 
       <template>
-        <v-bottom-sheet attach=".video-wrapper" v-model="sheet" hide-overlay>
+        <v-bottom-sheet
+          attach=".video-wrapper"
+          v-model="sheet"
+          hide-overlay
+        >
           <v-sheet>
             <div class="controls">
               <v-btn depressed color="primary" @click="togglePlay()">
@@ -18,7 +29,7 @@
           </v-sheet>
         </v-bottom-sheet>
       </template>
-    </div>
+    </v-container>
   </div>
 </template>
 
@@ -26,7 +37,7 @@
 export default {
   data() {
     return {
-      sheet: true,
+      sheet: false,
     };
   },
   computed: {
@@ -36,12 +47,17 @@ export default {
   },
   methods: {
     togglePlay() {
-      // if (this.video.paused || this.video.ended) {
-      //   this.video.play();
-      // } else {
-      //   this.video.pause();
-      // }
-      this.sheet = !this.sheet;
+      if (this.video.paused || this.video.ended) {
+        this.video.play();
+      } else {
+        this.video.pause();
+      }
+    },
+    showControls() {
+      this.sheet = true;
+    },
+    hideControls() {
+      setTimeout(() => this.sheet = false, 0)
     },
   },
 };
@@ -49,19 +65,18 @@ export default {
 
 <style>
 .video-wrapper {
-  border: 3px solid rgb(112, 29, 29);
-  background-color: blue;
+  background-color: rgb(83, 82, 82);
   transform: translate(0px, 0px);
   overflow: hidden;
-  width: 400px;
-  background-color: #666;
+
 }
 .controls {
-  border: 2px solid rgb(29, 30, 112);
+  padding: 0px;
+  
   width: 100%;
 }
 video {
   width: 100%;
-  height: 100%;
+  height: 50%;
 }
 </style>
