0963800b55b8278b3d2654a3ba6e754032e306c8
alekalek <alek@spectra-alarm.com>
2021-03-05 15:12:34 +0100

    added vuex plugin

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
