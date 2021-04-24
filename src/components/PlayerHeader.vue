0e879f8863ee505567a99272da00928f97d4efd2
alek98 <alek@spectra-alarm.com>
2021-04-24 10:46:06 +0200

    header created

--- src/components/PlayerHeader.vue
+++ src/components/PlayerHeader.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="overlay">
     <div class="heading">
-      <v-icon color="white">mdi-arrow-left</v-icon>
-      <div> asdf </div>
-      <div> {{getVideoName}} </div>
+      <!-- 
+       any button element is triggering slider label,
+       so I had to create div like a button.
+       Problem in Vuetify. 
+      -->
+      <div class="btn" @click="backToHomeScreen()">
+        <v-icon>mdi-chevron-left</v-icon>
+      </div>
+      <div class="video-name">{{ getVideoName }}</div>
     </div>
   </div>
 </template>
@@ -14,17 +20,23 @@ export default {
 
   data() {
     return {
-      videoName: 'haha'
-    }
+      videoName: "haha",
+    };
   },
 
   computed: {
     getVideoName() {
-      let fileName = this.videoPath.split('/').pop();
-      fileName = fileName.split('.')[0];
-      fileName = fileName.replace(/%20/g, ' ');
+      let fileName = this.videoPath.split("/").pop();
+      fileName = fileName.split(".")[0];
+      fileName = fileName.replace(/%20/g, " ");
       return fileName;
-    }
+    },
+  },
+
+  methods: {
+    backToHomeScreen() {
+      console.log("go back");
+    },
   },
 
   // watch: {
@@ -33,18 +45,42 @@ export default {
   //     this.videoName = newVal;
   //   }
   // }
-}
+};
 </script>
 
 <style>
 .overlay {
-  background: linear-gradient(to top, rgba(255, 255, 255, 0), rgba(0,0,0,.4) 20%, rgba(0,0,0,.8));
-  width: 100vw; 
+  background: linear-gradient(to top, rgba(255, 255, 255, 0), rgba(0,0,0,.3) 50%, rgba(0,0,0,.8));
+  width: 100vw;
 }
 .heading {
-  color:white;
+  color: white;
   padding: 1rem 1rem;
-  display: flex
-  
+  padding-bottom: 4rem;
+  text-shadow: 1px 1px 1px rgb(0 0 0 / 50%);
+  display: flex;
+}
+.video-name {
+  padding: 0rem 1.4rem;
+  margin: auto 0rem;
+  font-size: 1.3rem;
+}
+
+.heading .v-icon {
+  --size: 30px;
+  height: var(--size);
+  font-size: var(--size);
+  width: var(--size);
+  color: white;
+}
+.btn {
+  cursor: pointer;
+  border-radius: 50%;
+}
+.btn:hover {
+  background-color: rgba(245, 245, 245, 0.195);
+}
+.btn:active {
+  background-color: rgba(245, 245, 245, 0.295);
 }
 </style>
